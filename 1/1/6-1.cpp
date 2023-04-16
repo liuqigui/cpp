3616a07cbfac05f82cef17b29bec8d33324f633a
liuqigui <18531700930@163.com>
2023-04-16 23:17:54 +0800

    0416

---
+++ 1/1/6-1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+void swap(int a, int b);
+
+int main() {
+	cout << "helloworld" << endl;
+
+	int a = 10, b = 20;
+
+	swap(a, b);
+
+	return 0;
+}
---
+++ 1/1/swap.cpp
@@ -0,0 +1,12 @@
+#include"swap.h"
+
+void swap(int a, int b) {
+	int temp;
+	temp = a;
+	a = b;
+	b = temp;
+
+	cout << "a=" << a << endl;
+	cout << "b=" << b << endl;
+
+}
